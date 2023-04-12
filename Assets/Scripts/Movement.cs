f52aa2ac77b960294c951d2dca2502e625340389
Rosa Choi <momorosa.design@gmail.com>
2023-04-12 16:46:38 -0700

    Renamed the scene and created framerate independent rocket thrust movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,43 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float mainThrust = 100f;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("up");
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("Pressed RIGHT ARROW - Turning Right");
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("Pressed Left ARROW - Turning Left");
+        }
+    }
+}
+ 
